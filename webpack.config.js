4fe09eec57622205433315a8b81a56c269fdba55
Jungmin Cha <ckwjdals90@gmail.com>
2017-03-09 21:15:43 -0800

    debugged webpack.config.js, and created index.js and App.js backbone

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
-module.exports ={
+module.exports = {
   entry: './src/index.js',
 
   output: {
-    path: __dirname + '/public',
+    path: __dirname + '/public/',
     filename: 'bundle.js'
+  },
+
+  devServer: {
+    inline: true,
+    port: 9999,
+    contentBase: __dirname + '/public/'
   },
 
   module: {
     loaders: [
       {
-        text: /\.jsx?$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
+          cacheDirectory: true,
           presets: ['react', 'es2015']
         }
       }
