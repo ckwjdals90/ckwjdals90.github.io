07322cd351e9502dfb1f8e51274ed51d829b5bd1
Jungmin Cha <ckwjdals90@gmail.com>
2017-03-11 12:15:04 -0800

    configured routes for the created view components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
 import App from './App';
+import Home from './containers/Home';
+import About from './containers/About';
+import Portfolio from './containers/Portfolio';
+import Contact from './containers/Contact';
 
 import './index.css';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home} />
+      <Route path="about" component={About} />
+      <Route path="portfolio" component={Portfolio} />
+      <Route path="contact" component={Contact} />
+    </Route>
+  </Router>,
+  rootElement
+);
