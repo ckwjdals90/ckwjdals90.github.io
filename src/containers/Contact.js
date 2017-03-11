9cfd852bcae1ae5983956e1e86ca999748d21819
Jungmin Cha <ckwjdals90@gmail.com>
2017-03-11 10:53:28 -0800

    created view container components for Home, About, Portfolio, and Contact pages

---
+++ src/containers/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BigText from '../components/BigText';
+
+const Contact = () => {
+  return (
+    <div>
+      <BigText>Contact</BigText>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/containers/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BigText from '../components/BigText';
+
+const Portfolio = () => {
+  return (
+    <div>
+      <BigText>Portfolio</BigText>
+    </div>
+  );
+};
+
+export default Portfolio;
